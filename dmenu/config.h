e7b07212029e1f2e7bcda54d0bce22c1c9fb33f1
Adriel Estoller <adrielestoller@gmail.com>
2025-04-04 15:26:59 -0300

    Add again xresources

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static char font[] = "IBM Mono:style=medium:size=11";
 static const char *fonts[] = {
 	font,
 	"monospace:size=10",
